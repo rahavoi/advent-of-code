f49356def8118b8fb0b7b9076f6677b83f67fd00
Illia Rahavoi <illia.rahavoi@veem.com>
2020-12-03 11:13:35 -0500

    Day 3

---
+++ src/main/java/com/advent/of/code/_2020/Day3.java
@@ -0,0 +1,52 @@
+package com.advent.of.code._2020;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Day3
+{
+    public static void main(String[] args) throws Exception {
+        Day3 day3 = new Day3();
+        char[][] map = day3.getMap();
+        int[][] slopes = {{3,1}};
+        int result = 1;
+
+        for(int[] slope : slopes){
+            result *= day3.getTreeCount(map, slope[0], slope[1]);
+        }
+
+        System.out.println(result);
+    }
+
+    private char[][] getMap() throws IOException {
+        List<String> input = Files.readAllLines(Paths.get("src/main/resources/2020/Day3.txt"));
+        char[][] map = new char[input.size()][input.get(0).length()];
+
+        for(int i = 0; i < input.size(); i++){
+            map[i] = input.get(i).toCharArray();
+        }
+
+        return map;
+    }
+
+    private int getTreeCount(char[][] map, int stepsRight, int stepsDown) {
+        int x = 0;
+        int y = 0;
+        int treeCount = 0;
+
+        while(y < map.length - 1){
+            x+= stepsRight;
+            y += stepsDown;
+
+            x = x % map[0].length;
+
+            if(map[y][x] == '#'){
+                treeCount++;
+            }
+        }
+
+        return treeCount;
+    }
+}
