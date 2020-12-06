e83293bcfa874099375fe958c4d11b22f589585f
Illia Rahavoi <illia.rahavoi@veem.com>
2020-12-06 13:54:13 -0500

    Days 5,6

---
+++ src/main/java/com/advent/of/code/_2020/Day6.java
@@ -0,0 +1,79 @@
+package com.advent.of.code._2020;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Day6
+{
+    public static void main(String[] args) throws Exception{
+        part1();
+        part2();
+    }
+
+    private static void part1() throws IOException
+    {
+        List<List<Set<Character>>> allGroups = getAnswerGroups();
+
+        int sum = 0;
+        for(List<Set<Character>> group : allGroups){
+
+            Set<Character> superSet = group.get(0);
+
+            for(int i = 1; i < group.size(); i++){
+                superSet.addAll(group.get(i));
+            }
+
+            sum += superSet.size();
+        }
+
+        System.out.println(sum);
+    }
+
+    private static void part2() throws IOException{
+        List<List<Set<Character>>> allGroups = getAnswerGroups();
+
+        int sum = 0;
+        for(List<Set<Character>> gr : allGroups){
+            Set<Character> common = gr.get(0);
+
+            for(int i = 1; i < gr.size(); i++){
+                common = common.stream()
+                    .filter(gr.get(i)::contains)
+                    .collect(Collectors.toSet());
+            }
+
+            sum+=common.size();
+        }
+
+        System.out.println(sum);
+    }
+
+    private static List<List<Set<Character>>> getAnswerGroups() throws IOException
+    {
+        List<List<Set<Character>>> allGroups = new ArrayList<>();
+
+        List<Set<Character>> group = new ArrayList<>();
+
+        for(String line : Files.readAllLines(Paths.get("src/main/resources/2020/Day6.txt"))){
+            if(line.trim().isEmpty()){
+                allGroups.add(group);
+                group = new ArrayList<>();
+            } else {
+                Set<Character> answer = new HashSet<>();
+                group.add(answer);
+                for(char c : line.toCharArray()){
+                    answer.add(c);
+                }
+            }
+        }
+
+        allGroups.add(group);
+        return allGroups;
+    }
+}
