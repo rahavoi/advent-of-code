4a18ba4726621ad02c04667b1f8ce07ac7e3e8d7
Illia Rahavoi <illia.rahavoi@veem.com>
2021-12-02 10:36:49 -0500

    2021:1,2

---
+++ src/main/java/com/advent/of/code/_2021/Constants.java
@@ -0,0 +1,5 @@
+package com.advent.of.code._2021;
+
+public interface Constants {
+    String PATH_2021 =  "/Users/illiarahavoi/work/projects/advent-of-code/src/main/resources/2021/";
+}
---
+++ src/main/java/com/advent/of/code/_2021/Day1.java
@@ -0,0 +1,24 @@
+package com.advent.of.code._2021;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Day1 {
+    public static void main(String[] args) throws IOException{
+        List<String> lines = Files.readAllLines(Paths.get(Constants.PATH_2021 + "Day1.txt"));
+
+        long p1 = IntStream.range(0, lines.size() -1 )
+            .filter(i -> Integer.parseInt(lines.get(i)) < Integer.parseInt(lines.get(i + 1)))
+            .count();
+
+        long p2 = IntStream.range(2, lines.size() -1 )
+            .filter(i -> Integer.parseInt(lines.get(i - 2)) < Integer.parseInt(lines.get(i + 1)))
+            .count();
+
+        System.out.println(p1);
+        System.out.println(p2);
+    }
+}
